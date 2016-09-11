c3b3e49c6bc85aa1448104343d87e6d3b21bf8b2
ashleyg28 <ang97@nau.edu>
2016-09-10 23:11:36 -0700

    add basic functionality to read and write file

--- ComputerGraphicsProject1/main.c
+++ ComputerGraphicsProject1/main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[])
+{
+    char tgStr[25];
+    //create image pointer and "open", "read" into program
+    FILE * imgPtr = fopen("image.ppm", "r");
+    //gather the first character from the image file
+    int chr = fgetc(imgPtr);
+    //if the character isn't equal to p, then it isn't a ppm file
+    //other "errors" will go here
+    if(chr != 'P')
+    {
+        //output error message to standard error
+        fprintf(stderr, "Type is not PPM. \n");
+    }
+
+    //read file line by line
+    while(fgets(tgStr, 25, imgPtr))
+    {
+//          printf(tgStr);
+        if(imgPtr = 6)
+        {
+            //printf("test\n");
+            imgPtr = fopen("image.ppm", "w");
+            fputs("test",imgPtr);
+            //printf(imgPtr);
+
+        }
+    }
+
+
+    fclose(imgPtr);
+
+    return 0;
+}
