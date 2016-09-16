3137beea94af9103f9e09a80b4b52c1df9fc8ad8
ashleyg28 <ang97@nau.edu>
2016-09-16 15:54:31 -0700

    add functionality for p3 - p6

--- ComputerGraphicsProject1/Untitled1.c
+++ ComputerGraphicsProject1/Untitled1.c
@@ -11,78 +11,136 @@ int main(int argc, char *argv[])
 
     //ignore argv[0]
     //input file to read from
-    FILE* input = fopen(argv[2], "r");
+    FILE* input = fopen(argv[2], "rb");
     //output file to write to
-    FILE* output = fopen(argv[3], "w");
+    FILE* output = fopen(argv[3], "wb");
 
-    int chr = fgetc(input);
+//    int chr = fgetc(input);
 
     //return error if files aren't there
     if(input == NULL)
     {
         fprintf(stderr, "ERROR:  File(s) does not exist. \n");
     }
-    else if(output == NULL)
-    {
-        fprintf(stderr, "ERROR:  File(s) does not exist. \n");
-    }
-    else if(chr != 'P')
+
+//    else if(chr != 'P')
+//    {
+//        fprintf(stderr, "ERROR: Type is not PPM. \n");
+//    }
+    int chr = fgetc(input);
+    if(chr != 'P')
     {
         fprintf(stderr, "ERROR: Type is not PPM. \n");
     }
     //p3 or p6 format
-    int format = atoi(argv[1]);
+    chr = fgetc(input);
+//    printf("%c", chr);
 
-    if(format == 3)
+    if(chr == '3')
     {
         //p3 to p6
-//        printf("test 1\n");
-        width = 819;
-        height = 460;
-        size_t size = width * height * 3;
-        pixBuffer = (unsigned char *)malloc(size);
+        printf("P3 format found\n");
+        //read in next number
+        //file handle, format specifier, address of value
+        //comments
+        /*
+            #created by gimp \n
+            c = fgetc()
+            if(c=="#"){while(){ fgetcs until I come to new line}}
+        */
+//        int chr = fgetc(input);
+        fgetc(input); //to do: skip whitespace
+        //check for comments after any number
+        char c = fgetc(input);
+        if(c == '#')
+        {
+            while(c != '\n')
+            {
+                //c = fgetc
+                c = fgetc(input);
+
+            }
+        }
+        //width
+        int width;
+        fscanf(input, "%d",&width);
+        //height
+        int height;
+        fscanf(input, "%d", &height);
+//        printf("height =%d\n", height);
+        //max number
+        int maxNum;
+        fscanf(input, "%d", &maxNum);
+//        printf("maxNum =%d\n", maxNum);
+        fgetc(input); //hack for whitespace - go back and make while loop for header
+
+        //pixel buffer
+        unsigned int *pixBuffer;
+        pixBuffer = (unsigned int *)malloc(width * height * 3);
+        //for loop i = width * height * depth
+        int i;
+        for(i = 0; i < width * height * 3; i ++)
+        {
+            fscanf(input, "%d", pixBuffer);
+//            fwrite(pixBuffer, sizeof(int), sizeof(pixBuffer), output);
+        }
+//        printf("%d", pixBuffer);
+        int c = fgetc(input);
+        int i = 0;
+        while(c = ' ')
+        {
+            pixBuffer[i] = c;
+            i += 1;
+        }
+
+
+
+
 
 //        while(input != EOF)
 //        {
-//            newFile = fputc(pixBuffer, output);
-//
+//            fscanf(pixBuffer, 1, sizeof(pixBuffer), input);
+//        }
+//        fscanf(input, "%d", &pixBuffer);
+//        while()
+//        {
 //
 //        }
-
     }
 
-    if(format == 6)
-    {
-        //p6 to p3
-//        printf("test 2\n");
-//        int size = atoi(pixBuffer);
-//        printf(pixBuffer);
-//        width = 819;
-//        height = 460;
-//        size_t size = width * height * 3;
-        pixBuffer = (unsigned char* )malloc(sizeof(input));
-        //counter
-        int i = 0;
-        int c = fgetc(input);
-        while(c == ' ')
-        {
-//            pixBuffer[i] = c;
-//            c = fgetc(input);
-//            i += 1;
-//              result = fread(pixBuffer, 1, sizeof(pixBuffer), input);
-              fprintf(input, "%d", output);
-              fprintf(input, "%s", "hello", output);
-              fputc(pixBuffer,output);
+//    if(format == 6)
+//    {
+//        //p6 to p3
+////        printf("test 2\n");
+////        int size = atoi(pixBuffer);
+////        printf(pixBuffer);
+////        width = 819;
+////        height = 460;
+////        size_t size = width * height * 3;
+//        pixBuffer = (unsigned char* )malloc(sizeof(input));
+//        //counter
+//        int i = 0;
+//        int c = fgetc(input);
+//        while(c == ' ')
+//        {
+////            pixBuffer[i] = c;
+////            c = fgetc(input);
+////            i += 1;
+////              result = fread(pixBuffer, 1, sizeof(pixBuffer), input);
+//              fprintf(input, "%d", output);
+//              fprintf(input, "%s", "hello", output);
+////              fputc(pixBuffer,output);
+////
+//        }
 //
-        }
-
-        fprintf(output, "%c", pixBuffer);
-
-
-
-    }
+//        fprintf(output, "%c", pixBuffer);
+//
+//
+//
+//    }
 
     fclose(input);
-    fclose(output);
+//    fclose(output);
+//    fprintf(stderr, "Program terminated\n");
     return 0;
 }
--- ComputerGraphicsProject1/ppmrw.c
+++ ComputerGraphicsProject1/ppmrw.c
@@ -1,282 +1,97 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main(int argc, char *argv[])
-//{
-//
-//    //input file to read from
-//    FILE* input = fopen(argv[2], "r");
-//    //buffer declaration
-//    char *pixBuffer;
-//    //output file to write to
-//    FILE* output = fopen(argv[3], "w");
-//    //declare character variable
-//    int ch;
-//    //while there isn't a P character and it isn't the end of the input file...
-//    while((ch = fgetc(input)) != EOF && ch !='P');
-//    //if the end of the file is reached...
-//    if(ch == EOF)
-//        {
-//            //if incorrect file type or error reading from file, produce error
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "Incorrect file type\n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//        //while the end of file hasn't been reached and file type is not 3 or 6
-//        while((ch = fgetc(input)) != EOF && ch !='3' && ch != '6');
-//        if(ch == EOF)
-//        {
-//            //if incorrect file type or error reading from file, produce error
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "Incorrect file type\n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//
-//        //"magic number"
-//        int magicNum = ch;
-//        //to check whitespace
-//        while((ch = fgetc(input)) != EOF && isspace(ch));
-//        if(ch == EOF)
-//        {
-//
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached\n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//
-//        //declare width variable
-//        int width;
-//        //transverse horizontal
-//        int status = fscanf(input, "%d", &width);
-//        if(status == EOF)
-//        {
-//
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached \n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//
-//        if(status < 1 )
-//        {
-//            fprintf(stderr, "Invalid argument\n");
-//        }
-//
-//        int height;
-//        int sHeight = fscanf(input, "%d", &height);
-//        while((ch = fgetc(input)) != EOF && isspace(ch));
-//        if(ch == EOF)
-//        {
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached \n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//            return 1;
-//
-//        }
-//
-////        int height;
-////        int status = fscanf(input, "%d", &height);
-//        if(sHeight == EOF)
-//        {
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached \n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//
-//        if(status < 1 )
-//        {
-//            fprintf(stderr, "Invalid argument\n");
-//        }
-//
-//        int maxColor;
-//        int sColor = fscanf(input, "%d", &maxColor);
-//        if(sColor == EOF)
-//        {
-//            //copy for the other stuff
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached \n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//
-//        if(status < 1 )
-//        {
-//            fprintf(stderr, "Invalid argument\n");
-//        }
-//
-//        if(ch == EOF)
-//        {
-//            if(feof(input))
-//            {
-//                fprintf(stderr, "End of file reached \n");
-//
-//            }
-//            else if(ferror(input))
-//            {
-//                fprintf(stderr, "Error reading file\n");
-//            }
-//
-//            return 1;
-//
-//        }
-//        else if(isspace(ch)){
-//            fprintf(stderr, "Error reading file\n");
-//        }
-//
-//        int i;
-//        int j;
-//        size_t size = width * height * 3;
-//        pixBuffer = (unsigned char *)malloc(size);
-//        if(ch == '6'){
-//            fscanf(input, "%d", &(pixBuffer[(i * width + j)*3 + 1]));
-//                    while((ch = fgetc(input)) != EOF && isspace(ch));
-//                    if(ch == EOF)
-//                    {
-//                        if(feof(input))
-//                        {
-//                            fprintf(stderr, "End of file reached \n");
-//
-//                        }
-//                        else if(ferror(input))
-//                        {
-//                            fprintf(stderr, "Error reading file\n");
-//                        }
-//                        return 1;
-//
-//                    }
-//                    while((ch != EOF && isspace(ch))){
-//                        fread(pixBuffer, 1, size, input);
-//                        fwrite(input, 1, sizeof(input), output);
-//                    }
-//        }
-//        else if(ch == '3'){
-//            for(i = 0; i < width; i ++){
-//                for(j = 0; j < height; j++){
-//                    //this is red
-//                    //read in file to buffer
-//                    fscanf(input, "%d", &(pixBuffer[(i * width + j)*3]));
-//                    while((ch = fgetc(input)) != EOF && isspace(ch));
-//                    if(ch == EOF)
-//                    {
-//                        if(feof(input))
-//                        {
-//                            fprintf(stderr, "End of file reached \n");
-//
-//                        }
-//                        else if(ferror(input))
-//                        {
-//                            fprintf(stderr, "Error reading file\n");
-//                        }
-//                        return 1;
-//
-//                    }
-//                    //green
-//                    fscanf(input, "%d", &(pixBuffer[(i * width + j)*3 + 1]));
-//                    while((ch = fgetc(input)) != EOF && isspace(ch));
-//                    if(ch == EOF)
-//                    {
-//                        if(feof(input))
-//                        {
-//                            fprintf(stderr, "End of file reached \n");
-//
-//                        }
-//                        else if(ferror(input))
-//                        {
-//                            fprintf(stderr, "Error reading file\n");
-//                        }
-//
-//                        return 1;
-//
-//                    }
-//                    //blue
-//                    fscanf(input, "%d", &(pixBuffer[(i * width + j)*3 +2]));
-//                    while((ch = fgetc(input)) != EOF && isspace(ch));
-//                    if(ch == EOF)
-//                    {
-//                        //copy for the other stuff
-//
-//                        if(feof(input) && i < (width-1) && j < (height - 1))
-//                        {
-//                            fprintf(stderr, "P is not a thing \n");
-//
-//                        }
-//                        else if(ferror(input))
-//                        {
-//                            fprintf(stderr, "DENIED\n");
-//                        }
-//
-//                        return 1;
-//
-//                    }
-//                }
-//                //write "new" file to output.ppm
-//                fprintf(output,"%d", magicNum);
-//                fprintf(output,"%d", width);
-//                fprintf(output, "%d", height);
-//                fprintf(output, "%d", pixBuffer);
-//            }
-//        }
-//
-//        fclose(input);
-//        fclose(output);
-//        return 0;
-//
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    //declare pixel buffer
+    char *pixBuffer;
+    int width;
+    int height;
+
+    //ignore argv[0]
+    //input file to read from
+    FILE* input = fopen(argv[2], "rb");
+    //output file to write to
+    FILE* output = fopen(argv[3], "wb");
+
+//    int chr = fgetc(input);
+
+    //return error if files aren't there
+    if(input == NULL)
+    {
+        fprintf(stderr, "ERROR:  File(s) does not exist. \n");
+    }
+
+//    else if(chr != 'P')
+//    {
+//        fprintf(stderr, "ERROR: Type is not PPM. \n");
+//    }
+    int chr = fgetc(input);
+    if(chr != 'P')
+    {
+        fprintf(stderr, "ERROR: Type is not PPM. \n");
+    }
+    //p3 or p6 format
+    chr = fgetc(input);
+//    printf("%c", chr);
+
+    if(chr == '3')
+    {
+        //p3 to p6
+        printf("P3 format found\n");
+        //read in next number
+        //file handle, format specifier, address of value
+        //comments
+        /*
+            #created by gimp \n
+            c = fgetc()
+            if(c=="#"){while(){ fgetcs until I come to new line}}
+        */
+//        int chr = fgetc(input);
+        fgetc(input); //to do: skip whitespace
+        //check for comments after any number
+        char c = fgetc(input);
+        if(c == '#')
+        {
+            while(c != '\n')
+            {
+                //c = fgetc
+                c = fgetc(input);
+
+            }
+        }
+        //width
+        int width;
+        fscanf(input, "%d",&width);
+        //height
+        int height;
+        fscanf(input, "%d", &height);
+
+        //max number
+        int maxNum;
+        fscanf(input, "%d", &maxNum);
+
+        fgetc(input); //hack for whitespace - go back and make while loop for header
+        //pixel buffer
+        unsigned char *pixBuffer;
+        pixBuffer = malloc(width * height * 3);
+        //for loop i = width * height * depth
+        int i;
+        for(i = 0; i < width * height * 3; i ++)
+        {
+            fscanf(input, "%d", &pixBuffer[i]);
+        }
+        fclose(input);
+
+        //header
+        fprintf(output, "P6 %d %d %d ", width, height, 255);
+        fwrite(pixBuffer, sizeof(unsigned char), width * height * 3, output);
+
+        fclose(output);
+
+    }
+
+
+    return 0;
+}
